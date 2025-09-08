2e3e3b1c23aa1780dcda4ac1873004a177f89609
deepanshu-baghel <bagheldeepanshu7451@gmail.com>
2025-09-08 21:56:26 +0530

    navbar start

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import{Routes , Route } from 'react-router-dom';
 import Home from './pages/home';
 import BuyCredit from './pages/buycredit';
 import Result from './pages/result';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-
+    <div className='px-4 sm:px-20 md:px-32 lg:px-44 xl:px-52 2xl:px-60
+    min-h-screen
+    bg-gradient-to-b from-teal-50 to-orange-50
+    '>
+      <Navbar />
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/buycredit' element={ <BuyCredit/> } />
         <Route path='/result' element={ <Result/> } />
-        <Home />
-      <BuyCredit />
-      <Result />
       </Routes>
       
     </div>
